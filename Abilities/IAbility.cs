173033eda78a908e91fd35fc04810400bfcd8706
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-03-31 02:42:40 +0300

    add: ability and predicate

---
+++ Abilities/IAbility.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace HECSFramework.Core
+{
+    public interface IAbility : IHavePredicates, IHaveOwner, IHaveComponents, ICanAddComponents, IHaveSystems
+    {
+        void Execute<T>(T owner, T target = default, bool enable = true) where T : IEntity;
+        void SetupAbilityData(List<IComponent> components, List<ISystem> systems, List<IPredicate> predicates);
+    }
+
+    public interface IHaveComponents
+    {
+        bool TryGetHecsComponent<T>(out T component) where T : IComponent;
+    }
+
+    public interface ICanAddComponents
+    {
+        void AddHecsComponent(IComponent component);
+    }
+
+    public interface IHaveSystems
+    {
+        bool TryGetHecsSystem<T>(out T system) where T : ISystem;
+    }
+
+    public interface IHavePredicates
+    {
+        bool IsReady(IEntity target);
+    }
+}
---
+++ Predicate.cs
@@ -0,0 +1,30 @@
+﻿namespace HECSFramework.Core
+{
+    public interface IPredicate : IHaveOwner
+    {
+        bool IsReady(IEntity target);
+        void Init(IEntity owner, IAbility ability);
+    }
+
+    public abstract class BasePredicate : IPredicate
+    {
+        public IEntity Owner { get; set; }
+        protected IAbility ability { get; set; }
+
+        public void Init(IEntity owner, IAbility ability)
+        {
+            Owner = owner;
+            this.ability = ability;
+            LocalInit(owner);
+        }
+
+        public abstract void LocalInit(IEntity owner);
+
+        public abstract bool IsReady(IEntity target);
+    }
+
+    public interface IPredicateContainer
+    {
+        IPredicate GetPredicate { get; }
+    }
+}
