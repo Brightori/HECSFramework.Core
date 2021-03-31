e4dfc133e89889585d11ed885f0fcb2ad8c063bc
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-03-31 16:50:56 +0300

    upd: generator

--- HECSGenerator/CodeGenerator.cs
+++ HECSGenerator/CodeGenerator.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace HECSFramework.Core
+namespace HECSFramework.Core.Generator
 {
     public partial class CodeGenerator
     {
