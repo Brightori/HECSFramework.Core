5e534f993fea1752a57c631ee7f79e08a5f616ca
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-04-10 05:29:49 +0300

    add: exec abil sys

---
+++ IExecuteAbilitySystem.cs
@@ -0,0 +1,8 @@
+﻿
+namespace HECSFramework.Core
+{
+    public interface IExecuteAbilitySystem : ISystem
+    {
+        void Execute(IEntity owner = null, IEntity target = null, bool enable = true);
+    }
+}
