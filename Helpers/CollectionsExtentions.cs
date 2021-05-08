458aafc461431b3e5e797a8df87585f780d07274
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-05-08 06:44:23 +0300

    add: dictionary extention AddOrReplace

--- Helpers/CollectionsExtentions.cs
+++ Helpers/CollectionsExtentions.cs
@@ -4,6 +4,14 @@ namespace HECSFramework.Core.Helpers
 {
     public static class CollectionsExtentions
     {
+        public static void AddOrReplace<T,U>(this Dictionary<T,U> dictionary, T key,  U value)
+        {
+            if (dictionary.ContainsKey(key))
+                dictionary[key] = value;
+            else
+                dictionary.Add(key, value);
+        }
+
         public static bool AddOrRemoveElement<T>(this List<T> list, T element, bool add)
         {
             if (add)
