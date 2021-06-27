e02002d8c9dfcc6a2e36fbcf8ebe7394be851091
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-06-28 02:56:38 +0300

    add: modifiers

---
+++ Modifiers/IModifier.cs
@@ -0,0 +1,20 @@
+﻿namespace HECSFramework.Core
+{
+    public interface IModifier 
+    {
+        ModifierCalculationType GetCalculationType { get; }
+    }
+
+    public interface IModifier<T> : IModifier
+    {
+        T Modify(ref T value);
+    }
+
+    public enum ModifierCalculationType
+    {
+        Add = 0,
+        Subtract = 1,
+        Multiply = 2,
+        Divide = 3,
+    }
+}
---
+++ Modifiers/ModifiersContainer.cs
@@ -0,0 +1,45 @@
+﻿using HECSFramework.Core.Helpers;
+using System.Collections.Generic;
+
+namespace HECSFramework.Core
+{
+    public class ModifiersContainer<T, U> where U : struct where T : IModifier<U>
+    {
+        private readonly Dictionary<int, HashSet<T>> modifiers = new Dictionary<int, HashSet<T>>()
+        {
+            {(int)ModifierCalculationType.Add, new HashSet<T>()},
+            {(int)ModifierCalculationType.Subtract, new HashSet<T>()},
+            {(int)ModifierCalculationType.Multiply, new HashSet<T>()},
+            {(int)ModifierCalculationType.Divide, new HashSet<T>()},
+        };
+
+        public void AddModifier(T modifier)
+        {
+            modifiers[(int)modifier.GetCalculationType].AddOrRemoveElement(modifier, true);
+        }
+
+        public void RemoveModifier(T modifier)
+        {
+            modifiers[(int)modifier.GetCalculationType].AddOrRemoveElement(modifier, false);
+        }
+
+        public U GetCalculatedValue(in U value)
+        {
+            U currentMod = value;
+
+            foreach (var valueMod in modifiers[(int)ModifierCalculationType.Add])
+                valueMod.Modify(ref currentMod);
+
+            foreach (var valueMod in modifiers[(int)ModifierCalculationType.Subtract])
+                valueMod.Modify(ref currentMod);
+
+            foreach (var valueMod in modifiers[(int)ModifierCalculationType.Multiply])
+                valueMod.Modify(ref currentMod);
+
+            foreach (var valueMod in modifiers[(int)ModifierCalculationType.Divide])
+                valueMod.Modify(ref currentMod);
+
+            return currentMod;
+        }
+    }
+}
