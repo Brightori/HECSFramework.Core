583cc5d7d79ce80503b8f45e69c0bb94f33c4be2
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-07-22 14:43:13 +0300

    upd: rename appl to app

--- Components/AppVersionComponent.cs
+++ Components/AppVersionComponent.cs
@@ -5,13 +5,9 @@ namespace Components
 {
     [Serializable]
     [Documentation("GameLogic", "Компонент в котором мы храним версию")]
-    public class ApplVersionComponent : BaseComponent
+    public class AppVersionComponent : BaseComponent
     {
         [Field(0)]
-        public string Prefix = "a";
-        [Field(1)]
         public int Version = 101;
-        [Field(2)]
-        public string Suffix = "a";
     }
 }
