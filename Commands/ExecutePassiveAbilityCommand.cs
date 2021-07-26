c68ada577f8ad7dfad2fefa9bfac6464d43aa787
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-07-26 15:20:17 +0300

    add: execute passive abilities command

---
+++ Commands/ExecutePassiveAbilityCommand.cs
@@ -0,0 +1,12 @@
+﻿using HECSFramework.Core;
+
+namespace Commands
+{
+    [Documentation("Ability", "Команда которой активируем пассивные абилки, её надо отправлять абилковладельцу")]
+    public struct ExecutePassiveAbilityCommand : ICommand
+    {
+        public IEntity Target;
+        public IEntity Owner;
+        public bool Enabled;
+    }
+}
