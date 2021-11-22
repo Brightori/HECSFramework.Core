d20aa456db1c9a7fd48b1aa2b37ce3842a28e488
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-11-22 05:35:44 +0300

    add: identifiers

---
+++ identifiers.cs
@@ -0,0 +1,12 @@
+﻿namespace HECSFramework.Core
+{
+    public interface IIdentifier
+    {
+        int Id { get; }
+    }
+
+    public class Identifier : IIdentifier
+    {
+        public int Id { get; set; }
+    }
+}
