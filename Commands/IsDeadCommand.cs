ad5c088c36a91b98d455ac9a51e1015477e638f3
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-12-13 05:16:52 +0300

    fix: documentation and add isdead command

---
+++ Commands/IsDeadCommand.cs
@@ -0,0 +1,9 @@
+﻿using HECSFramework.Core;
+
+namespace Commands
+{
+	[Documentation(Doc.Character, "Команда которую мы отправляем локально у актора, что он мертв")]
+	public struct IsDeadCommand : ICommand
+	{
+	}
+}
--- DocumentationFeature/DocHelper.cs
+++ DocumentationFeature/DocHelper.cs
@@ -1,28 +1,27 @@
-namespace HECSFramework.Documentation
+//если нужны проектозависимые/свои хелперы для документации, то делаем в проект свой парт класс
+public static partial class Doc
 {
-    //если нужны проектозависимые/свои хелперы для документации, то делаем в проект свой парт класс
-    public static partial class Doc
-    {
-        public const string Character = "Character";
-        public const string Camera = "Camera";
-        public const string Quests = "Quests";
-        public const string NPC = "NPC";
-        public const string Tag = "Tag";
-        public const string GameLogic = "GameLogic";
-        public const string Player = "Player";
-        public const string Location = "Location";
-        public const string Scenarios = "Scenarios";
-        public const string Visual = "Visual";
-        public const string Battle = "Battle";
-        public const string Strategy = "Strategy";
-        public const string Rewards = "Rewards";
-        public const string Input = "Input";
-        public const string Sound = "Sound";
-        public const string Ability = "Ability";
-        public const string UI = "UI";
-        public const string AI = "AI";
-        public const string NONE = "NONE";
-        public const string State = "State";
-        public const string Animation = "Animation";
-    }
+    public const string Character = "Character";
+    public const string Camera = "Camera";
+    public const string Quests = "Quests";
+    public const string NPC = "NPC";
+    public const string Tag = "Tag";
+    public const string GameLogic = "GameLogic";
+    public const string Player = "Player";
+    public const string Location = "Location";
+    public const string Scenarios = "Scenarios";
+    public const string Visual = "Visual";
+    public const string Battle = "Battle";
+    public const string Strategy = "Strategy";
+    public const string Rewards = "Rewards";
+    public const string Input = "Input";
+    public const string Sound = "Sound";
+    public const string Ability = "Ability";
+    public const string UI = "UI";
+    public const string AI = "AI";
+    public const string NONE = "NONE";
+    public const string State = "State";
+    public const string Animation = "Animation";
 }
+
+namespace HECSFramework.Documentation { }
