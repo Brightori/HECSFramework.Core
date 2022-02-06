11833442e0a2eb1d251a57672076f164bf250126
Eugen Dubovik <e.dubovik@kefirgames.com>
2022-02-06 08:54:34 +0300

    upd: hecs debug now have default realization

--- Debugger/HECSDebug.cs
+++ Debugger/HECSDebug.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Diagnostics;
 
 namespace HECSFramework.Core
 {
     public static class HECSDebug
     {
-        public static IDebugDispatcher Dispatcher { get; private set; }
+        public static IDebugDispatcher Dispatcher { get; private set; } = new NullDebug();
 
         public static void Init(IDebugDispatcher debugDispatcher)
         {
@@ -30,21 +31,45 @@ namespace HECSFramework.Core
         {
             Dispatcher.LogDebug(info.ToString(), context);
         }
-        
+
         public static void Log(object info)
         {
             Dispatcher.Log(info.ToString());
         }
-        
+
         public static void LogWarning(object info)
         {
             Dispatcher.LogWarning(info.ToString());
         }
-        
+
         public static void LogError(object info)
         {
             Dispatcher.LogError(info.ToString());
         }
+    }
+}
+
+//заглушка 
+public class NullDebug : IDebugDispatcher
+{
+    public void Log(string info)
+    {
+        Console.WriteLine(info);
+    }
+
+    public void LogDebug(string info, object context)
+    {
+        Console.WriteLine(info);
+    }
+
+    public void LogError(string info)
+    {
+        Console.WriteLine(info);
+    }
+
+    public void LogWarning(string info)
+    {
+        Console.WriteLine(info);
     }
 }
 
