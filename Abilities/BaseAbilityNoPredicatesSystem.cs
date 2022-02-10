64aaf70a106a239b6bd0d05d32c64e18f73f422c
Eugen Dubovik <e.dubovik@kefirgames.com>
2022-02-10 11:45:03 +0300

    rem: codogen dependency

--- Abilities/BaseAbilityNoPredicatesSystem.cs
+++ Abilities/BaseAbilityNoPredicatesSystem.cs
@@ -5,12 +5,16 @@ namespace HECSFramework.Core
 {
     public abstract class BaseAbilityNoPredicatesSystem : BaseSystem, IAbilitySystem, IReactCommand<ExecuteAbilityCommand>
     {
-        private HECSMask predicateMask = HMasks.GetMask<AbilityPredicateComponent>();
+        private HECSMask actorContainerIDMask = HMasks.GetMask<ActorContainerID>();
 
         public void CommandReact(ExecuteAbilityCommand command)
         {
             Execute(command.Owner, command.Target, command.Enabled);
-            Owner.Command(new AbilityWasExecutedCommand { AbilityIndex = Owner.GetActorContainerID().ContainerIndex });
+            
+            Owner.Command(new AbilityWasExecutedCommand
+            {
+                AbilityIndex = Owner.GetHECSComponent<ActorContainerID>(ref actorContainerIDMask).ContainerIndex
+            });
         }
 
         public abstract void Execute(IEntity owner = null, IEntity target = null, bool enable = true);
