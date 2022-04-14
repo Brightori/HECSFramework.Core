80126148f60ddcba4fed0ed62562a21746c2a327
Eugen Dubovik <e.dubovik@kefirgames.com>
2022-04-14 19:19:49 +0300

    mov: remove counter  modifier command to core

---
+++ Counters/Commands/RemoveCounterModifierCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using HECSFramework.Core;
+
+namespace Commands
+{
+    public struct RemoveCounterModifierCommand<T> : ICommand where T : struct
+    {
+        public int Id;
+        public Guid Owner;
+        public IModifier<T> Modifier;
+    }
+}
