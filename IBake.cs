f294a923eb1fe8bb9db64ba26880d078f108d1db
DESKTOP-11GN4NS\User <evgenii.dubovik@playstrom.io>
2022-06-30 15:14:26 +0300

    rem: unity part of baking

--- IBake.cs
+++ IBake.cs
@@ -1,16 +1,10 @@
-﻿using HECSFramework.Unity;
-
-namespace HECSFramework.Core
+﻿namespace HECSFramework.Core
 {
-    [Documentation(Doc.HECS, Doc.Baking, "This interface we using for transfering data to server for example, we run this interface on baking process, on realization of this interface we should gather data from enviroment")]
-    public interface IBake
+    public partial interface IBake
     {
-        public void Bake(EntityContainer entityContainer);
     }
 
-    [Documentation(Doc.HECS, Doc.Baking, "This interface we using for transfering data to server for example, we run this interface before main baking process, on realization of this interface we should gather data from enviroment")]
-    public interface IPreBake
+    public partial interface IPreBake
     {
-        public void PreBake(EntityContainer entityContainer);
     }
 }
