a80d89464ab139e9078b6013f8a3d2defc831a17
evgenii.dubovik@playstrom.io <evgenii.dubovik@playstrom.io>
2023-03-09 11:16:32 +0300

    upd: actor provider now on core too, bcz we need similar behaviour for destroying entity on server and client

---
+++ Components/ActorProviderComponent.cs
@@ -0,0 +1,9 @@
+﻿using HECSFramework.Core;
+
+namespace Components
+{
+    [Documentation(Doc.HECS, "this component provide Actor from actor monobehaviour to systems and components")]
+    public sealed partial class ActorProviderComponent : BaseComponent
+    {
+    }
+}
