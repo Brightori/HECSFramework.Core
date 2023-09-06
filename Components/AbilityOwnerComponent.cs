37c5ae48a82eb0010ecd2f9a1914a05434d8a261
Brightori <el.brightori@gmail.com>
2023-09-07 01:41:46 +0300

    add: non serialized to ability owner component

--- Components/AbilityOwnerComponent.cs
+++ Components/AbilityOwnerComponent.cs
@@ -7,6 +7,7 @@ namespace Components
     [Documentation(Doc.Abilities, Doc.HECS, "This component is required for the ability, the character that owns it is thrown into the owner")]
     public class AbilityOwnerComponent : BaseComponent
     {
+        [NonSerialized]
         public Entity AbilityOwner;
     }
 }
