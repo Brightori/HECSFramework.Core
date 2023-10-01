16e76894d9fd1c4bd7ca0d56b6694b267187f429
Brightori <el.brightori@gmail.com>
2023-10-01 03:41:48 +0300

    add: entitycontainer dropdown attr

---
+++ Helpers/EntityContainerDropDownAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HECSFramework.Core;
+
+namespace Helpers
+{
+    [Documentation(Doc.Attributes, "Provide DropDown list of specified identifiers")]
+    public class EntityContainerDropDownAttribute : Attribute
+    {
+        public readonly string tagComponentName;
+
+        public EntityContainerDropDownAttribute(string tagComponentName)
+        {
+            this.tagComponentName = tagComponentName;
+        }
+    }
+}
