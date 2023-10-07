39976956ee643df218bca7714b46c60caddbc48e
Brightori <el.brightori@gmail.com>
2023-10-07 20:55:22 +0300

    upd: game state component

--- Features/GamePhases/GameStateComponent.cs
+++ Features/GamePhases/GameStateComponent.cs
@@ -5,7 +5,7 @@ namespace Components
 {
     [Serializable]
     [Documentation(Doc.GameState, "this component hold index of current game state, we use it at game state system")]
-    public sealed class GameStateComponent : BaseComponent, IWorldSingleComponent
+    public sealed partial class GameStateComponent : BaseComponent, IWorldSingleComponent
     {
         public int CurrentState { get; private set; }
         public int PreviousState { get; private set; }
@@ -18,6 +18,26 @@ namespace Components
         {
             PreviousState = CurrentState;
             CurrentState = index;
+        }
+
+        public bool IsNeededState(int index1)
+        {
+            return (CurrentState == index1);
+        }
+
+        public bool IsNeededState(int index1, int index2) 
+        { 
+            return IsNeededState(index1) || IsNeededState(index2);
+        }
+
+        public bool IsNeededState(int index1, int index2, int index3)
+        {
+            return IsNeededState(index1, index2) || IsNeededState(index3);
+        }
+
+        public bool IsNeededState(int index1, int index2, int index3, int index4)
+        {
+            return IsNeededState(index1, index2) || IsNeededState(index3, index4);
         }
     }
 }
