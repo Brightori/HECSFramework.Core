15cbeb841351e2d0042b00848ff584ec59112b3c
Brightori <el.brightori@gmail.com>
2023-10-15 14:23:33 +0300

    add: generic after command

---
+++ Commands/AfterCommand.cs
@@ -0,0 +1,10 @@
+﻿using HECSFramework.Core;
+
+namespace Commands
+{
+    [Documentation(Doc.HECS, Doc.Helpers, Doc.Global, "we use this command when we need send universal context or build chain of commands, we can send inside this command other command")]
+    public struct AfterCommand<T> : IGlobalCommand
+    {
+        public T Value;
+    }
+}
