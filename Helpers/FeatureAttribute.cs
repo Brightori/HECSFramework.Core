3b3aa8f88f9a361459947e5ae8ff19296b173e48
Brightori <el.brightori@gmail.com>
2023-11-15 07:44:19 +0300

    add: feature attribute

---
+++ Helpers/FeatureAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HECSFramework.Core
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class FeatureAttribute : Attribute
+    {
+        public readonly string[] Features;
+
+        public FeatureAttribute(params string[] features)
+        {
+            Features = features;
+        }
+    }
+}
