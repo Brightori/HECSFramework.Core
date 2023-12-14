90e0d30e9d7fbc59461b05a74892c845ce6ba508
Brightori <el.brightori@gmail.com>
2023-12-14 03:11:17 +0300

    add: strategies map

--- Helpers/EntityContainersMap.cs
+++ Helpers/EntityContainersMap.cs
@@ -4,3 +4,8 @@ public static partial class EntityContainersMap
 {
     public static readonly Dictionary<int, string> EntityContainersIDtoString;      
 }
+
+public static partial class StrategiesMap
+{
+    public static readonly Dictionary<int, string> StrategiesIDtoString;
+}
