f7ff2b122f554d4077ad8fae9d06099c536f90c4
Brightori <el.brightori@gmail.com>
2024-02-25 17:25:26 +0300

    upd: index generator

--- IndexGenerator.cs
+++ IndexGenerator.cs
@@ -11,20 +11,27 @@ namespace HECSFramework.Core
 
         public static int GetIndexForType(Type c)
         {
+            if (c == null)
+                return 0;
+
             var typeName = c.Name;
             return GetIndexForType(typeName);
         }
 
         public static int GetIndexForType(string typeName)
         {
+            if (string.IsNullOrEmpty(typeName))
+                return 0;
+
             var lenght = typeName.Length;
-            int index = lenght + typeName[0].GetHashCode();
+
+            int index = lenght + typeName[0].GetHashCode() + typeName[^1].GetHashCode();
             int hash = 10070531;
 
             for (int i = 0; i < lenght; i++)
             {
-                int charC = typeName[i].GetHashCode()*i;
-                index += (charC + (101161*(i+3)) + (hash ^ (charC*i)));
+                int charC = typeName[i].GetHashCode() * i;
+                index += (charC + (101161 * (i + 3)) + (hash ^ (charC * i)));
             }
 
             return index;
