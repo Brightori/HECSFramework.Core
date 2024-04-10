56ff61f57b9e5ad8ef73054e63a362bdb963002d
Brightori <el.brightori@gmail.com>
2024-04-11 01:42:44 +0300

    add: documentation

--- Features/GamePhases/StopGameState.cs
+++ Features/GamePhases/StopGameState.cs
@@ -2,7 +2,11 @@
 
 namespace Commands
 {
-    [Documentation(Doc.GameLogic, Doc.GameState, "we send this command when we need stop state")]
+	/// <summary>
+	/// its service command
+	/// if u want stop state manualy, u should use end state command instead of this
+	/// </summary>
+    [Documentation(Doc.GameLogic, Doc.GameState, "we send this command when we need stop state, this is service command, u dont need send it manualy, we send it from main game state system")]
     public struct StopGameStateGlobalCommand : IGlobalCommand
 	{
 		public int GameState;
