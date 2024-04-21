c33a9d91238da93ac0bc7e42101ca9b384c3bfa8
Evgenii Dubovik <dubovik.evgenii@beresnev.games>
2024-04-21 16:30:08 +0300

    add: wait for state job

---
+++ Features/GamePhases/WaitForStateJob.cs
@@ -0,0 +1,26 @@
+﻿using Components;
+using HECSFramework.Core;
+
+namespace Systems
+{
+    public struct WaitForStateJob : IHecsJob
+    {
+        public World World;
+        public int State;
+
+        public WaitForStateJob(World world, int state)
+        {
+            World = world;
+            State = state;
+        }
+
+        public bool IsComplete()
+        {
+            return World.GetSingleComponent<GameStateComponent>().CurrentState == State;
+        }
+
+        public void Run()
+        {
+        }
+    }
+}
