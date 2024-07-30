107dd19461559f7bca806465b607f3c923d833f4
Brightori <el.brightori@gmail.com>
2024-07-30 22:31:14 +0300

    rem: legacy rewards add: reward tag upd: name of base reward interface

---
+++ Components/RewardTagComponent.cs
@@ -0,0 +1,9 @@
+﻿using HECSFramework.Core;
+
+namespace Components
+{
+    [Documentation(Doc.Tag, Doc.Rewards, "we mark rewards containers by this tag")]
+    public sealed class RewardTagComponent : BaseComponent
+    {
+    }
+}
---
+++ Rewards/IRewardSystem.cs
@@ -0,0 +1,11 @@
+﻿using Commands;
+using HECSFramework.Core;
+
+namespace HECSFramework.Rewards
+{
+   [Documentation(Doc.Rewards, "base system for rewards")]
+    public interface IRewardSystem : IReactCommand<ExecuteReward>
+    {
+        
+    }
+}
