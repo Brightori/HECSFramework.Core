59844369d3b94db1c1a2e54bb3573d985f00886e
Brightori <el.brightori@gmail.com>
2024-08-20 01:32:28 +0300

    add: async action

---
+++ Actions/AsyncActionsHolderComponent.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Cysharp.Threading.Tasks;
+using HECSFramework.Core;
+using Helpers;
+
+namespace Components
+{
+    [Serializable]
+    [Documentation(Doc.HECS, Doc.Abilities, "this component holds actions with identifier, we can call visual actions or execite composite actions by this component")]
+    public sealed partial class AsyncActionsHolderComponent : AsyncBaseActionsHolderComponent
+    {
+    }
+
+    public abstract partial class AsyncBaseActionsHolderComponent : BaseComponent, IDisposable, IAsyncActionsHolderComponent
+    {
+        protected HECSList<AsyncActionsToIdentifier> Actions = new HECSList<AsyncActionsToIdentifier>(4);
+        private Stack<HECSList<UniTask>> readyForPool = new Stack<HECSList<UniTask>>();
+        private HECSList<(int index, HECSList<UniTask> uniTasks)> inProgress = new HECSList<(int index, HECSList<UniTask> uniTasks)>(2);
+        private int index;
+
+        public void Dispose()
+        {
+            foreach (var a in Actions)
+            {
+                foreach (var action in a.Actions)
+                {
+                    if (action is IDisposable disposable)
+                        disposable.Dispose();
+                }
+            }
+        }
+
+        public async UniTask ExecuteAction(int Index, Entity to, Entity from = null)
+        {
+            index++;
+
+            var pool = GetList(index);
+
+            for (int i = 0; i < Actions.Count; i++)
+            {
+                if (Actions[i].ID == Index)
+                {
+                    var subPool = GetList(index);
+
+                    foreach (var a in Actions[i].Actions)
+                    {
+                        subPool.Add(a.ActionAsync(to, from));
+                    }
+
+                    pool.Add(UniTask.WhenAll(subPool));
+                }
+            }
+
+            if (pool.Count > 0)
+                await UniTask.WhenAll(pool);
+
+            Release(index);
+        }
+
+        public void Release(int index)
+        {
+            var pool = HECSPooledArray<(int index, HECSList<UniTask> uniTasks)>.GetArray(inProgress.Count);
+
+            foreach (var kp in inProgress)
+            {
+                if (kp.index == index)
+                {
+                    pool.Add(kp);
+                    readyForPool.Push(kp.uniTasks);
+                }
+            }
+
+            for (int i = 0; i < pool.Count; i++)
+            {
+                inProgress.RemoveSwap(pool.Items[i]);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private HECSList<UniTask> GetList(int currentIndex)
+        {
+            if (readyForPool.TryPop(out var list))
+            {
+                inProgress.Add((currentIndex, list));
+                return list;
+            }
+
+            var newList = new HECSList<UniTask>(2);
+            inProgress.Add((currentIndex, newList));
+            return newList;
+        }
+    }
+
+    public interface IAsyncActionsHolderComponent
+    {
+        UniTask ExecuteAction(int Index, Entity to, Entity from = null);
+    }
+
+    public struct AsyncActionsToIdentifier
+    {
+        public int ID;
+        public List<IAsyncAction> Actions;
+    }
+}
--- Actions/IAction.cs
+++ Actions/IAction.cs
@@ -1,7 +1,20 @@
-﻿namespace HECSFramework.Core
+﻿using Cysharp.Threading.Tasks;
+
+namespace HECSFramework.Core
 {
     public interface IAction
     {
         public void Action(Entity entity);
+    }
+
+    public interface IAsyncAction
+    {
+        /// <summary>
+        /// here we provide target and owner
+        /// </summary>
+        /// <param name="to">target</param>
+        /// <param name="from">owner</param>
+        /// <returns></returns>
+        public UniTask ActionAsync(Entity to, Entity from = null);
     }
 }
