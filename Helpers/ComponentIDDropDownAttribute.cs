0df52c3c1d30102ec1411d86403b1745c355a638
Brightori <el.brightori@gmail.com>
2024-11-17 13:41:18 +0300

    add: drop down attribute for components ids

---
+++ Helpers/ComponentIDDropDownAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using HECSFramework.Core;
+
+namespace Helpers
+{
+    [Documentation(Doc.Attributes, "Provide DropDown list of сomponent indeces")]
+    public class ComponentIDDropDownAttribute : Attribute
+    {
+        public ComponentIDDropDownAttribute()
+        {
+        }
+    }
+}
