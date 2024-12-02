f2fca0d7f9eb615b3e24a608f5a917e85066fbb6
Brightori <el.brightori@gmail.com>
2024-12-02 22:53:33 +0300

    upd: documentation add: ihave position

---
+++ Features/SharedFuncs/IHavePosition.cs
@@ -0,0 +1,13 @@
+﻿namespace HECSFramework.Core
+{
+    public interface IHavePosition
+    {
+#if UNITY_2017_1_OR_NEWER
+        UnityEngine.Vector3 GetPosition { get; }
+#else
+
+        System.Numerics.Vector3 GetPosition { get; } 
+#endif
+    }
+}
+
