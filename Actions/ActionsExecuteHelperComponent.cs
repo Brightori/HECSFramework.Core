683c519ce66aee15bceb31a321fc004741a0bddc
Brightori <el.brightori@gmail.com>
2025-02-24 01:20:35 +0300

    add: actions execute helper

---
+++ Actions/ActionsExecuteHelperComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using HECSFramework.Core;
+
+namespace Components
+{
+    public sealed class ActionsExecuteHelperComponent : BaseComponent
+    {
+        private IActionsHolderComponent[] actionsHolders;
+
+        public override void AfterInit()
+        {
+            actionsHolders = Owner.GetComponentsByType<IActionsHolderComponent>().ToArray();
+        }
+
+        public void ExecuteAction(int id, Entity owner)
+        {
+            foreach (var action in actionsHolders)
+            {
+                action.ExecuteAction(id, owner);
+            }
+        }
+    }
+}
