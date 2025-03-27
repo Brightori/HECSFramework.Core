6a9ce6062474b1445c0d41c65b2b4403998a4f02
Brightori <el.brightori@gmail.com>
2025-03-27 21:44:26 +0300

    add: add ability func and parent component

---
+++ Abilities/Commands/AddAbilityCommand.cs
@@ -0,0 +1,10 @@
+﻿using HECSFramework.Core;
+
+namespace Commands
+{
+    [Documentation(Doc.Commands, Doc.Abilities, "we send this command when what start scenarion of adding command")]
+    public struct AddAbilityCommand : ICommand
+    {
+        public Entity Entity;
+    }
+}
