68449631407c6a660eaf152c5fe718b5fe34f657
Brightori <el.brightori@gmail.com>
2025-07-13 03:43:55 +0300

    add: play animation command for some cases

---
+++ Commands/Animation/PlayAnimationCommand.cs
@@ -0,0 +1,12 @@
+﻿using HECSFramework.Core;
+
+namespace Commands
+{
+    [Documentation(Doc.Animation, "this command play animation by hash of state, u should definitly now name of state")]
+    public struct PlayAnimationCommand : ICommand
+    {
+        public int Index;
+        public int Layer;
+        public float NormalizedTime;
+    }
+}
