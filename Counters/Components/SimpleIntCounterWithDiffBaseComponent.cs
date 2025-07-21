dff6753dfb134d3ffdcb1dbfdd875de1a2777dd7
Brightori <el.brightori@gmail.com>
2025-07-22 02:45:52 +0300

    add: simple component counter with diff command

---
+++ Counters/Components/SimpleIntCounterWithDiffBaseComponent.cs
@@ -0,0 +1,28 @@
+﻿using Commands;
+using HECSFramework.Core;
+
+namespace Components
+{
+    [Documentation(Doc.HECS, Doc.Counters, "Simple int counter component with sending command on change value")]
+    public abstract class SimpleIntCounterWithDiffBaseComponent : BaseComponent, ICounter<int>
+    {
+        public abstract int Value { get; protected set; }
+        public abstract int Id { get; }
+
+        public void ChangeValue(int value)
+        {
+            Owner.Command(new DiffCounterCommand<int>()
+            {
+                Id = Id,
+                PreviousValue = Value,
+                Value = Value + value
+            });
+            Value += value;
+        }
+
+        public void SetValue(int value)
+        {
+            Value = value;
+        }
+    }
+}
