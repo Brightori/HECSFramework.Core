2b8ef540942545dd725aab3ef7097a5255791585
Brightori <el.brightori@gmail.com>
2025-09-03 17:13:34 +0300

    upd: ihave  position

--- Features/SharedFuncs/IHavePosition.cs
+++ Features/SharedFuncs/IHavePosition.cs
@@ -1,7 +1,10 @@
 ﻿namespace HECSFramework.Core
 {
-    public interface IHavePosition
+    public interface IHavePosition 
     {
+        //in monobehaviours u should make it false on destroy
+        public bool IsAlive { get; }
+
 #if UNITY_2017_1_OR_NEWER
         UnityEngine.Vector3 GetPosition { get; }
 #else
